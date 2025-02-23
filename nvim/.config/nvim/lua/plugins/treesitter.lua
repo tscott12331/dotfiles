5d28451f1100cf152dd2e244fa3c32400b013e7d
Thomas Scott <t.scott12331@gmail.com>
2025-02-23 12:18:33 -0800

    added new theme + made syntax highlighting on by default

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,8 +1,17 @@
-local M = {
+return {
     "nvim-treesitter/nvim-treesitter",
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            ensure_installed = { "c", "lua", "vim", "rust", "tsx", "sql"},
+            auto_install = true,
+            highlight = {
+                enable = true,
+            }
+        }
+
+    end,
 }
 
-return { M }

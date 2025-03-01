98ac90246516ad75e2a142f95fb762282f702dad
Thomas Scott <thomas.scott.837@my.csun.edu>
2025-02-28 18:55:00 -0800

    removed csharp from required lsp (who needs that anyways)

--- nvim/.config/nvim/lua/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/plugins/lsp/mason.lua
@@ -21,7 +21,6 @@ return {
             ensure_installed = { 
                 "lua_ls",
                 "rust_analyzer",
-                "csharp_ls",
                 "cssls",
                 "svelte",
                 "ts_ls",

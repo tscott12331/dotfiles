c2002b6e6e744f99e1344f75a57ab5fc17c05f8b
Thomas Scott <t.scott12331@gmail.com>
2025-11-11 20:15:51 -0800

    Add neotree

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,9 @@
--- oil keymaps
+-- oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
--- telescope keymaps
+-- telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', function() builtin.find_files() end, {})
+
+-- neotree
+vim.keymap.set('n', '<leader>fd', '<Cmd>Neotree toggle<CR>')
